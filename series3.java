abd8a5b65c32395d83657e219046bebba4d55bd4
Naincy <111273192+Naincy04@users.noreply.github.com>
2022-12-11 16:51:22 +0530

    Add files via upload
    Write a program to display the sum of the series :- 1 + (1*2) + (1*2*3) + ........ upto 10 terms.

---
+++ series3.java
@@ -0,0 +1,12 @@
+public class series3 {
+    public static void main(String args[])
+    {
+        int p=1,s=0;
+        for(int i=1; i<=3;i++)
+        {
+            p=p*i;
+            s=s+p;
+        }
+        System.out.print(s);
+    }
+}
